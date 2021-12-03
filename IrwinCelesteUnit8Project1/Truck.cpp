689f72e13ef02a16fce6d8a812cfcadd1d76763d
Celeste Irwin <celeste.irwin@sinclair.edu>
2021-12-02 23:31:24 -0500

    Added Truck class and defined member variable, and function prototypes

---
+++ IrwinCelesteUnit8Project1/Truck.cpp
@@ -0,0 +1,8 @@
+//
+//  Truck.cpp
+//  IrwinCelesteUnit8Project1
+//
+//  Created by Celeste Irwin on 12/2/21.
+//
+
+#include "Truck.hpp"
---
+++ IrwinCelesteUnit8Project1/Truck.hpp
@@ -0,0 +1,30 @@
+//
+//  Truck.hpp
+//  IrwinCelesteUnit8Project1
+//
+//  Created by Celeste Irwin on 12/2/21.
+//
+
+#ifndef Truck_hpp
+#define Truck_hpp
+
+#include <stdio.h>
+#include "Vehicle.hpp"
+
+using namespace std;
+
+class Truck : private Vehicle
+{
+private:
+    int towCap;
+public:
+    Truck()
+    {towCap=0;}
+    void setTowCap (int cap)
+    {towCap=cap;}
+    int getTowCap () const
+    {return towCap;}
+    void displayInfo() const;
+};
+
+#endif /* Truck_hpp */
